d1754fa0a0be40d997a83797add464f2ae214030
Stas Indyukov <stasun008@gmail.com>
2023-03-11 20:39:02 +0300

    added asm files

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+extern char* func(char *a);
+
+int main(void)
+{
+    char text[]="Ram pam pam";
+
+    printf("Input string      > %s\n", text);
+    char *r = func(text);
+    printf("Conversion results> %s\n", r);
+
     return 0;
 }
